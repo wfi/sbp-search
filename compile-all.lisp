3b64e8e81efeeaf1261742970754d10929fd0448
Wayne Iba <iba@westmont.edu>
2016-12-15 14:50:19 -0800

    added more files

---
+++ compile-all.lisp
@@ -0,0 +1,7 @@
+
+(compile-file "my-profiler")
+(compile-file "Ext-AStar-code-from-file-search-engine")
+(compile-file "Ext-AStar-file-based-SBP")
+(compile-file "SBP-Ext-AStar-blank-index-jimslide")
+(compile-file "slide-puzzle-inits")
+
---
+++ loader.lisp
@@ -0,0 +1,9 @@
+(load "my-profiler")
+(load "Ext-AStar-code-from-file-search-engine")
+(load "Ext-AStar-file-based-SBP")
+(load "SBP-Ext-AStar-blank-index-jimslide")
+(load "slide-puzzle-inits")
+
+
+(defparameter **path-to-file-storage** "./storage/")
+(defparameter **max-buffer-position-count** 5000000)
