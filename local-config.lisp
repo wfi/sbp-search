2e8235cda31b1727ba458c9a1c7810b891d69cb1
Wayne Iba <iba@westmont.edu>
2017-02-09 15:09:24 -0800

    remove local-config.lisp from git tracking

--- local-config.lisp
+++
@@ -1,6 +0,0 @@
-
-;; Set local configuration parameters here.
-;; This file is not to be tracked by git
-
-(defparameter **path-to-file-storage** "./storage/")
-(defparameter **max-buffer-position-count** 5000000)
