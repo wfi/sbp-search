fd1ff8c7fc1e96648d2936c606d536f96987e6eb
Wayne Iba <iba@westmont.edu>
2017-10-25 12:14:37 -0700

    fixed loader.lisp to look for local-config.lisp and added corresponding documentation

--- loader.lisp
+++ loader.lisp
@@ -4,6 +4,17 @@
 (load "SBP-Ext-AStar-blank-index-jimslide")
 (load "slide-puzzle-inits")
 
-(load "local-config.lisp") ;; NOT tracked by git, but containing local values
+;; reate your own local-config.lisp with settings for at least
+;; the two defparametrs below. **path-to-file-storage** should NOT
+;; be to a solid-state drive (SSD) and should have a trailing slash.
+;; If you want to automatically start a run, you should add the
+;; appropriate code to local-config. For example, you might add something like the
+;; sbp-setup-ext-astar and external-a-star function calls below to your local-config.lisp.
+;; Do NOT uncomment them here and commit that change.
 ;(defparameter **path-to-file-storage** "./storage/")
 ;(defparameter **max-buffer-position-count** 5000000)
+;
+;(sbp-setup-ext-astar 'climb12 'foo)
+;(external-a-star)
+
+(load "local-config.lisp") ;; NOT tracked by git, but containing local values
