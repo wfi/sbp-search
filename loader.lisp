267dd2d324ddcb5a346a06e7b291e696a04e923e
Wayne Iba <iba@westmont.edu>
2017-10-25 12:01:58 -0700

    Fixed loader.lisp to load local-config.lisp and added supporting documentation

--- loader.lisp
+++ loader.lisp
@@ -9,12 +9,17 @@
 (load "radix-file-buffers")
 (load "slide-puzzle-inits")
 
+;; create your own local-config.lisp with settings for at least
+;; the two defparamers below. **path-to-file-storage** should NOT
+;; be to a solid-state drive (SSD) and should have a trailing slash.
+;; If you want to automatically start a run, you should add the 
+;; appropriate code to local-config. For example, you might add something like the
+;; sbp-setup-ext-astar and external-a-star function calls below to your local-config.lisp.
+;; Do NOT uncomment them here and commit that change.
+;(defparameter **path-to-file-storage** "/space/bigspace/userspace/iba/sbp-files/")
+;(defparameter **max-buffer-position-count** 5000000)
+;
+;(sbp-setup-ext-astar 'climb12 'foo)
+;(external-a-star)
 
-(defparameter **path-to-file-storage** "storage")
-(defparameter **max-buffer-position-count** 5000000)
-
-(sbp-setup-ext-astar 'climb12 'foo)
-
-(external-a-star)
-
-
+(load "local-config.lisp")
